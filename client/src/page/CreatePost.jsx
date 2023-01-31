82317e94046986290bfe39b046a0882569b21acc
Raphael Ventura <raphaelventura55@gmail.com>
2023-01-31 16:02:04 +0100

    initial commit

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -21,12 +21,11 @@ const generateImage = async () => {
   if (form.prompt) {
     try {
       setGeneratingImg(true);
-      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+      const response = await fetch('https://dall-e-1a1r.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Origin': 'https://dalle-arbb.onrender.com',
         },
         body: JSON.stringify({
           prompt: form.prompt,
@@ -52,7 +51,7 @@ const handleSubmit = async (e) => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dall-e-1a1r.onrender.com/api/v1/post', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
